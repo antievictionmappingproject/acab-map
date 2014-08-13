aaedfb83f39726ff58f8700e5ec809556433d8e5
Matthew Gerring <gerring.matthew@gmail.com>
2014-08-12 18:52:53 -0700

    added python script for producing analysis data and added requirements.txt

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import csv
 from pymongo import MongoClient
 from datetime import datetime
+import json
 
 client = MongoClient()
 
@@ -38,6 +39,26 @@ def juveniles(year):
         { '$group' : {"_id":"$Descript", "count":{"$sum":1} } },
         { '$sort' : {'count':-1} }
     ])
+
+def resolution(year):
+    return incidents.aggregate([
+        { '$match' : {'Date': {'$gte':datetime(year,1,1),'$lte':datetime(year,12,31)} } },
+        { '$group' : {"_id":"$Resolution", "count":{"$sum":1} } },
+        { '$sort' : {'count':-1} }
+    ])
+
+def write_to_file(filename,data):
+    outfile = open('analysis/%s.json' % filename,'w+')
+    outjson = json.dumps(data)
+    outfile.write(outjson)
+    outfile.close()
+
+def do_stuff(func,filename):
+    outdata = {}
+    for year in range(2003,2015):
+        outdata[year] = globals()[func](year)['result']
+    write_to_file(filename, outdata)
+    
 
 """
 Quality of life crimes
